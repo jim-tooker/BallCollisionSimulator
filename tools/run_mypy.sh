bf1b75c5dfebcf766766ddf22feb2a676258ad1b
Jim Tooker <160680067+jim-tooker@users.noreply.github.com>
2024-08-06 21:11:03 -0700

    add vpython-stubs as a submodule

--- tools/run_mypy.sh
+++ tools/run_mypy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ..
-mypy ball_collision_sim.py test_ball_collision_sim.py test_cases.py --strict
+# Note: This script is meant to be run from the root project directory, like this `tools/run_mypy.sh`.
+mypy . --strict
